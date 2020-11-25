7434c68938250eaa728e6e8391ad7b722a22f2c5
李韻晟 <dreak603@gmail.com>
2020-11-25 12:33:49 +0800

    chore: temp commit

--- lib-kafka/src/main.rs
+++ lib-kafka/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use futures::*;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::message::{OwnedHeaders, Headers};
+use rdkafka::{ClientContext, TopicPartitionList, Message};
+use rdkafka::consumer::{ConsumerContext, Rebalance, StreamConsumer, Consumer, CommitMode};
+use rdkafka::error::KafkaResult;
+
+struct CustomContext;
+
+impl ClientContext for CustomContext {}
+
+impl ConsumerContext for CustomContext {
+    fn pre_rebalance(&self, rebalance: &Rebalance) {
+        println!("Pre rebalance {:?}", rebalance);
+    }
+
+    fn post_rebalance(&self, rebalance: &Rebalance) {
+        println!("Post rebalance {:?}", rebalance);
+    }
+
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
+        println!("Commiting offsets {:?}", result);
+    }
+}
+
+type LoggingConsumer = StreamConsumer<CustomContext>;
+
+async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
+    
 }
